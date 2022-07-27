87c2e9cedc1763f68905e1642d69c213d99da3c3
hauhuynh66 <hauhuynh66@gmail.com>
2022-07-27 17:59:46 +0700

    update

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,6 +1,5 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { RxStompService } from 'src/app/stomp.service';
 
 @Component({
   selector: 'app-chat',
@@ -11,18 +10,15 @@ import { RxStompService } from 'src/app/stomp.service';
 export class ChatComponent implements OnInit {
   messages : Array<String> = Array()
   message : FormControl = new FormControl("")
-  constructor(private rxStompService: RxStompService) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.rxStompService.watch('/topic/demo').subscribe((message) => {
-      this.messages.push(message.body);
-    });
+    
   }
 
   publish(){
-    const message = `Message generated at ${new Date()}`;
-    this.rxStompService.publish({ destination: '/topic/demo', body: message });
+    
   }
 
 }
